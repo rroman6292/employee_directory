698ed920d098f5a4065044d96150e3910850515d
Richie Roman <answer417@gmail.com>
2021-05-01 22:17:11 -0400

    changes

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './style.css'
+
+function Footer(){
+    return (
+        <footer>
+            <ul className="footer">
+                <li>Copyright © Richie Roman | All Rights Reserved</li>
+                <li>Users by Random User Generator</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Header(){
+    return (
+        <nav>
+            <h1>Employee Directory</h1>
+        </nav>
+    )
+}
+
+export default Header
