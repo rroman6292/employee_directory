5523b8c1f3f1ee9920600878db246c32cf8a4ea4
Richie Roman <answer417@gmail.com>
2021-05-01 22:41:04 -0400

    works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import MainContent from './components/MainContent'
+import Container from './components/Container'
 import './components/style.css'
 
 function App() {
@@ -10,10 +10,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MainContent />
+      <Container />
       <Footer />
     </div>
   );
 }
-
 export default App;
